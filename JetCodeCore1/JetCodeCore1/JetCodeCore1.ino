44cbbc8f6b4665fcf84404a77eb6759fb1afae79
jpavner13 <jpavner@gmail.com>
2023-03-13 14:33:58 -0600

    Initial Commit

---
+++ JetCodeCore1/JetCodeCore1/JetCodeCore1.ino
@@ -0,0 +1,7 @@
+// This core is only used for RC communications and flight control of the Jet
+
+void setup() {
+}
+
+void loop() {
+}
---
+++ JetCodeCore2/JetCodeCore2/JetCodeCore2.ino
@@ -0,0 +1,7 @@
+// This core is only used for sensor data and other operations not related to flight/movement of the Jet
+
+void setup() {
+}
+
+void loop() {
+}
