4220659e8a77e815edce784e1db99479c0a85dc9
jpavner13 <jpavner@gmail.com>
2023-03-13 18:24:39 -0600

    Basic MCU 6050 code

---
+++ JetCodeCore2/JetCodeCore2/MPU.cpp
@@ -0,0 +1,91 @@
+#include "MCU.h"
+
+void initializeMPU(Adafruit_MPU6050 mpu){
+    while (!mpu.begin()) {
+        Serial.println("Failed to find MPU-6050 chip. Trying again...");
+        delay(1000);
+    }
+    Serial.println("MPU-6050 Connected!");
+
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    Serial.print("Accelerometer range set to: ");
+    switch (mpu.getAccelerometerRange()) {
+        case MPU6050_RANGE_2_G:
+        Serial.println("+-2G");
+        break;
+        case MPU6050_RANGE_4_G:
+        Serial.println("+-4G");
+        break;
+        case MPU6050_RANGE_8_G:
+        Serial.println("+-8G");
+        break;
+        case MPU6050_RANGE_16_G:
+        Serial.println("+-16G");
+        break;
+    }
+
+    mpu.setGyroRange(MPU6050_RANGE_2000_DEG);
+    Serial.print("Gyro range set to: ");
+    switch (mpu.getGyroRange()) {
+    case MPU6050_RANGE_250_DEG:
+        Serial.println("+- 250 deg/s");
+        break;
+    case MPU6050_RANGE_500_DEG:
+        Serial.println("+- 500 deg/s");
+        break;
+    case MPU6050_RANGE_1000_DEG:
+        Serial.println("+- 1000 deg/s");
+        break;
+    case MPU6050_RANGE_2000_DEG:
+        Serial.println("+- 2000 deg/s");
+        break;
+    }
+
+    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+    Serial.print("Filter bandwidth set to: ");
+    switch (mpu.getFilterBandwidth()) {
+    case MPU6050_BAND_260_HZ:
+        Serial.println("260 Hz");
+        break;
+    case MPU6050_BAND_184_HZ:
+        Serial.println("184 Hz");
+        break;
+    case MPU6050_BAND_94_HZ:
+        Serial.println("94 Hz");
+        break;
+    case MPU6050_BAND_44_HZ:
+        Serial.println("44 Hz");
+        break;
+    case MPU6050_BAND_21_HZ:
+        Serial.println("21 Hz");
+        break;
+    case MPU6050_BAND_10_HZ:
+        Serial.println("10 Hz");
+        break;
+    case MPU6050_BAND_5_HZ:
+        Serial.println("5 Hz");
+        break;
+    }
+}
+
+void printMpuEvent(sensors_event_t a, sensors_event_t g, sensors_event_t temp){
+    Serial.print("Acceleration X: ");
+    Serial.print(a.acceleration.x);
+    Serial.print(", Y: ");
+    Serial.print(a.acceleration.y);
+    Serial.print(", Z: ");
+    Serial.print(a.acceleration.z);
+    Serial.println(" m/s^2");
+
+    Serial.print("Rotation X: ");
+    Serial.print(g.gyro.x);
+    Serial.print(", Y: ");
+    Serial.print(g.gyro.y);
+    Serial.print(", Z: ");
+    Serial.print(g.gyro.z);
+    Serial.println(" rad/s");
+
+    Serial.print("Temperature: ");
+    Serial.print(temp.temperature);
+    Serial.println(" degC");
+}
